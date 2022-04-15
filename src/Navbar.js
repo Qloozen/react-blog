150a35d1a70a46c22b3d29e31ce712122ceb0cc7
qloozen <qiangloozen@gmail.com>
2022-04-15 22:16:09 +0200

    Created nested components

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+// command "sfc" (stateless functional component) from simple react snippet
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'; // Import of a component-function
 
+// Displaying the components to the DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +11,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
