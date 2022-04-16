93c52b58f341de1c7210f07502db13be7f24d735
qloozen <qiangloozen@gmail.com>
2022-04-16 21:29:50 +0200

    Practice code with state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { toBeInTheDocument } from '@testing-library/jest-dom/dist/matchers';
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
-const Home = () => {
+import { useState } from "react";
+
+const Home = () => { 
+    // useState hook for making values reactive
+    // useState returns an array with the statefull value + a function to update it
+    // "const [name, setName] =" is called array destructuring (https://www.freecodecamp.org/news/array-and-object-destructuring-in-javascript/)
+    const [name, setName] = useState("Jan");
+    const [age, setAge] = useState(21);
+
+    const handleClick = () => {
+        setName("Peter");
+        setAge(30);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
      );
 }
