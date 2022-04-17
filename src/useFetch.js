3f01e4ac78a527738590ce7ee1ff7d9183d4121f
qloozen <qiangloozen@gmail.com>
2022-04-17 23:35:06 +0200

    Added an abort cleanup in the useFetch

--- src/useFetch.js
+++ src/useFetch.js
@@ -13,9 +13,13 @@ const useFetch = (url) => {
     // Adding an empty dependency array to useEffect() will only run at the first time
     // Adding a value to the array it will run only if that value changes
     useEffect(() => {
+        // When switching to different components before the fetch completed the state change, it will log an error.
+        // AbortController is used to prevent this
+        const abortController = new AbortController();
+
         //Setting up a fake json server with endpoints: npx json-server --watch data/db.json --port 8000 
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortController.signal})
             .then(res => {
                 if(!res.ok) {
                     throw new Error("Could not fetch the data for that resource")
@@ -28,11 +32,18 @@ const useFetch = (url) => {
                 setError(null);
             })
             .catch(e => {
-                setError(e.message)
-                setIsPending(false);
+                if (e.name === "AbortError") {
+                    console.log("fetch aborted")
+                } else {
+                    setError(e.message)
+                    setIsPending(false);
+                }
             })
-        }, 1000)
-    }, [url])
+        }, 1000);
+
+        return () => abortController.abort();
+
+    }, [url]);
 
     return {data, isPending, error}
 }
