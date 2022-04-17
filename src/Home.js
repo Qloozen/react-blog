64f8a22d041a4315055f498c6abfa77112b6fce2
qloozen <qiangloozen@gmail.com>
2022-04-17 14:39:26 +0200

    Added a fetch request to the fake json server

--- src/Home.js
+++ src/Home.js
@@ -5,32 +5,26 @@ const Home = () => {
     // useState hook for making values reactive
     // useState returns an array with the statefull value + a function to update it
     // "const [name, setName] =" is called array destructuring (https://www.freecodecamp.org/news/array-and-object-destructuring-in-javascript/)
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-
-    const [name, setName] = useState("Jan");
+    const [blogs, setBlogs] = useState(null);
 
     // "useEffect" hook will run everytime after every completed render
     // Adding an empty dependency array to useEffect() will only run at the first time
     // Adding a value to the array it will run only if that value changes
     useEffect(() => {
-        console.log("use effect ran")
-    }, [name])
+        //Setting up a fake json server with endpoints: npx json-server --watch data/db.json --port 8000 
+        fetch("http://localhost:8000/blogs")
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+            })
+    }, [])
 
     return ( 
         // "blogs={}" is a prop (arbitrary inputs)
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs"/>
-            <BlogList blogs={blogs.filter(blog => blog.author === "mario")} title="Mario" handleDelete={handleDelete}/>
-            <button onClick={() => {setName("Henk")}}>Change name</button>
+            {blogs && <BlogList blogs={blogs} title="All blogs"/>}
         </div>
      );
 }
