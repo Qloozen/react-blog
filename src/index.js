37d52600a109aa5671ad26b895b8b611d68d30ca
qloozen <qiangloozen@gmail.com>
2022-04-17 21:56:24 +0200

    Found related issue with react router v5

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import { BrowserRouter} from 'react-router-dom';
 
 // Displaying the components to the DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Issues with the react router v5 could've been caused by the strictmode (issue: url gets updated but not rendering the components)
+// I fixed it with the new react router v6 
+// Found source: https://github.com/facebook/react/issues/21674
 root.render(
   <React.StrictMode>
     <BrowserRouter>
