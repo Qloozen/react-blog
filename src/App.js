08660374e9d7e575ccd9bbe3f008c788229ba926
qloozen <qiangloozen@gmail.com>
2022-04-17 17:21:02 +0200

    Created base route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { toBeInTheDocument } from '@testing-library/jest-dom/dist/matchers';
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 // This is a component-function that returns JSX code
 function App() { 
   return ( 
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home/>
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
