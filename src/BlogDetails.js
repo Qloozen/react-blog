00627ba523dde3d3bff8383bc83360bf70860a87
qloozen <qiangloozen@gmail.com>
2022-04-18 13:46:12 +0200

    Added delete functionality to a blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useNavigate } from "react-router-dom";
 
 const BlogDetails = () => {
 
     const { id } = useParams();
     const {data: blogDetails, isPending, error} = useFetch(`http://localhost:8000/blogs/${id}`);
+    const navigate = useNavigate();
+
+    const handleDelete = () => {
+        fetch("http://localhost:8000/blogs/" + blogDetails.id, {
+            method: "DELETE"
+        })
+        .then(() => {
+            navigate("/");
+        })
+    }
 
     return (  
         <div className="blog-details">
@@ -15,6 +26,7 @@ const BlogDetails = () => {
                     <h2>{ blogDetails.title }</h2>
                     <p>Written by { blogDetails.author }</p>
                     <div>{ blogDetails.body }</div>
+                    <button onClick={ handleDelete }>delete</button>
                 </article>
             )}
         </div>
