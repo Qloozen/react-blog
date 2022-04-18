314ef39c96b9a875bf310120afb300c5aeb85b58
qloozen <qiangloozen@gmail.com>
2022-04-18 11:43:50 +0200

    added a form with state & empty submit handler

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,46 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("Peter");
+
+    const handleSubmit = (e) => {
+        e.preventDefault() // prevents refreshing after a submit
+        const blog = { title, body, author}
+    }
+
     return ( 
         <div className="create">
             <h2>Add a new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog Title</label>
+                <input 
+                    type="text" 
+                    required 
+                    value={title} 
+                    onChange={e => {setTitle(e.target.value)}}
+                />
+
+                <label>Blog Body</label>
+                <textarea 
+                    required 
+                    value={body} 
+                    onChange={e => {setBody(e.target.value)}}
+                >
+                </textarea>
+
+                <label>Blog Author</label>
+                <select 
+                    value={author} 
+                    onChange={e => {setAuthor(e.target.value)}}
+                >
+                    <option value="Jan">Jan</option>
+                    <option value="Peter">Peter</option>
+                </select>
+
+                <button>Add blog</button>
+            </form>
         </div>
      );
 }
