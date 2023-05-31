9502c15cba4b2afbf8f436633b5b74ce01b29314
Qiang Loozen <70981109+Qloozen@users.noreply.github.com>
2023-05-31 17:28:28 +0200

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>Blog</h1>
             <div className="links">
-                <Link to="/">Home tetst</Link>
+                <Link to="/">Home</Link>
                 <Link to="/create">New Blog</Link>
             </div>
         </nav>
